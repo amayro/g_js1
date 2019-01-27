14e4133f1a6fe0f29af97c84a4fbdab7edfd2502
amayro <aabmstu@gmail.com>
2019-01-27 23:54:14 +0300

    hw_06

---
+++ hw_06/gallery.js
@@ -0,0 +1,146 @@
+"use strict";
+
+/**
+ * @property {Object} settings Объект с настройками галереи.
+ * @property {string} settings.previewSelector Селектор обертки для миниатюр галереи.
+ * @property {string} settings.openedImageWrapperClass Класс для обертки открытой картинки.
+ * @property {string} settings.openedImageClass Класс открытой картинки.
+ * @property {string} settings.openedImageScreenClass Класс для ширмы открытой картинки.
+ * @property {string} settings.openedImageCloseBtnClass Класс для картинки кнопки закрыть.
+ * @property {string} settings.openedImageCloseBtnSrc Путь до картинки кнопки открыть.
+ * @property {element} openedImageEl Элемент открытой картинки.
+ */
+const gallery = {
+    settings: {
+        previewSelector: '.galleryPreviewsContainer',
+        openedImageWrapperClass: 'galleryWrapper',
+        openedImageClass: 'galleryWrapper__image',
+        openedImageScreenClass: 'galleryWrapper__screen',
+        openedImageCloseBtnClass: 'galleryWrapper__close',
+        openedImageBackBtnClass: 'galleryWrapper__back',
+        openedImageNextBtnClass: 'galleryWrapper__next',
+    },
+    openedImageEl: null,
+
+    /**
+     * Инициализирует галерею, ставит обработчик события.
+     * @param {Object} userSettings Объект настроек для галереи.
+     */
+    init(userSettings = {}) {
+        Object.assign(this.settings, userSettings);
+
+        document
+            .querySelector(this.settings.previewSelector)
+            .addEventListener('click', event => this.containerClickHandler(event));
+    },
+
+    /** Обработчик события клика для открытия картинки. */
+    containerClickHandler(event) {
+        if (event.target.tagName !== 'IMG') {
+            return;
+        }
+        this.openImage(event.target);
+    },
+
+    /**
+     * Открывает картинку.
+     * @param {target} target Картинка, на которую нажали.
+     */
+    openImage(target) {
+        this.openedImageEl = target;
+        const img = new Image();
+        let insertImage = this.getScreenContainer().querySelector(`.${this.settings.openedImageClass}`);
+
+        img.onload = () => insertImage.src = target.dataset.full_image_url;
+        img.onerror = () => insertImage.src = target.src;
+        img.src = target.dataset.full_image_url;
+
+
+    },
+
+    /**
+     * Возвращает контейнер для открытой картинки, либо создает такой контейнер, если его еще нет.
+     * @returns {Element}
+     */
+    getScreenContainer() {
+        const galleryWrapperElement = document.querySelector(`.${this.settings.openedImageWrapperClass}`);
+        if (galleryWrapperElement) {
+            if (galleryWrapperElement.getAttribute('hidden')) {
+                galleryWrapperElement.removeAttribute('hidden')
+            }
+            return galleryWrapperElement;
+        }
+
+        return this.createScreenContainer();
+    },
+
+    /**
+     * Создает контейнер для открытой картинки.
+     * @returns {HTMLElement}
+     */
+    createScreenContainer() {
+        const galleryWrapperElement = document.createElement('div');
+        galleryWrapperElement.classList.add(this.settings.openedImageWrapperClass);
+
+        const backImageElement = document.createElement('div');
+        backImageElement.classList.add(this.settings.openedImageBackBtnClass);
+        backImageElement.addEventListener('click', () => this.getPrevImage(this.openedImageEl));
+        galleryWrapperElement.appendChild(backImageElement);
+
+        const nextImageElement = document.createElement('div');
+        nextImageElement.classList.add(this.settings.openedImageNextBtnClass);
+        nextImageElement.addEventListener('click', () => this.getNextImage(this.openedImageEl));
+        galleryWrapperElement.appendChild(nextImageElement);
+
+        const closeImageElement = document.createElement('div');
+        closeImageElement.classList.add(this.settings.openedImageCloseBtnClass);
+        closeImageElement.addEventListener('click', () => this.close());
+        galleryWrapperElement.appendChild(closeImageElement);
+
+        const galleryScreenElement = document.createElement('div');
+        galleryScreenElement.classList.add(this.settings.openedImageScreenClass);
+        galleryWrapperElement.appendChild(galleryScreenElement);
+
+        const image = new Image();
+        image.classList.add(this.settings.openedImageClass);
+        image.addEventListener('click', () => this.getNextImage(this.openedImageEl));
+        galleryWrapperElement.appendChild(image);
+
+        document.body.appendChild(galleryWrapperElement);
+
+        return galleryWrapperElement;
+    },
+
+    /**
+     * Возвращает следующий элемент (картинку) от открытой или первую картинку в контейнере,
+     * если текущая открытая картинка последняя.
+     * @returns {Element} Следующую картинку от текущей открытой.
+     */
+    getNextImage(openImageEl) {
+        const nextImage = openImageEl.nextElementSibling
+            ? openImageEl.nextElementSibling
+            : openImageEl.parentElement.firstElementChild;
+        this.openImage(nextImage)
+    },
+
+    /**
+     * Возвращает предыдущий элемент (картинку) от открытой или последнюю картинку в контейнере,
+     * если текущая открытая картинка первая.
+     * @returns {Element} Предыдущую картинку от текущей открытой.
+     */
+    getPrevImage(openImageEl) {
+        const nextImage = openImageEl.previousElementSibling
+            ? openImageEl.previousElementSibling
+            : openImageEl.parentElement.lastElementChild;
+        this.openImage(nextImage)
+    },
+
+    /**
+     * Закрывает (прячет) контейнер для открытой картинки.
+     */
+    close() {
+        document.querySelector(`.${this.settings.openedImageWrapperClass}`).setAttribute('hidden', 'hidden');
+    }
+};
+
+window.onload = () => gallery.init();
---
+++ hw_06/script.js
@@ -0,0 +1,122 @@
+"use strict";
+
+/**
+ * @property {object} settings Настройки корзины товаров.
+ * @property {{price: number, name: string}[]} orders Список товаров что купил пользователь.
+ * @property {HTMLElement} basketCountEl Место для показа количества товаров.
+ * @property {HTMLElement} basketPriceEl Место для показа цены всех товаров.
+ */
+const basket = {
+    settings: {
+        containerSelector: '#container',
+        countSelector: '#basket-count',
+        priceSelector: '#basket-price',
+    },
+
+    orders: [],
+    currentObj: {
+        name: null,
+        price: null,
+        count: null,
+    },
+
+    /**
+     * Инициализирует переменные для корзины и показывает эти значения.
+     */
+    init(settings = {}) {
+        Object.assign(this.settings, settings);
+
+        document
+            .querySelector(this.settings.containerSelector)
+            .addEventListener('click', event => this.containerClickHandler(event));
+        this.render();
+    },
+
+    /** Обработчик события клика для покупки товара */
+    containerClickHandler(event) {
+        this.currentObj = {
+            name: event.target.dataset.name,
+            price: event.target.dataset.price,
+            count: 1,
+        };
+        if (event.target.classList.contains('buy-btn')) {
+            this.addOrder();
+        } else if (event.target.classList.contains('reduce-order-btn')) {
+            this.reduceOrder();
+        } else if (event.target.classList.contains('delete-order-btn')) {
+            this.deleteOrder();
+        }
+    },
+
+    /**
+     * Отображает количество всех товаров и их цену.
+     */
+    render() {
+        document.querySelector(this.settings.countSelector).innerText = this.getOrdersCount();
+        document.querySelector(this.settings.priceSelector).innerText = this.getOrdersPrice();
+    },
+
+    /**
+     * Считает и возвращает количество всех купленных товаров из массива this.orders.
+     * @returns {number} Число всех купленных товаров.
+     */
+    getOrdersCount() {
+        let count = 0;
+        this.orders.forEach((el) => count += el.count)
+        return count
+    },
+
+    /**
+     * Считает и возвращает цену всех купленных товаров из массива this.orders.
+     * @returns {number} Число всех купленных товаров.
+     */
+    getOrdersPrice() {
+        let price = 0;
+        this.orders.forEach((el) => price += el.price * el.count);
+        return price
+    },
+
+    /**
+     * Добавляет купленный товар в массив купленных товаров и отображает количество и цену всех товаров.
+     */
+    addOrder() {
+        if (this.orders.some(el => el.name === this.currentObj.name)) {
+            this.orders.forEach((el) => el.name === this.currentObj.name ? el.count++ : el.count);
+        } else {
+            console.log('not');
+            this.orders.push(this.currentObj)
+        }
+        this.render();
+    },
+
+    /**
+     * Уменьшает число заказов товара из корзины и отображает количество и цену всех товаров.
+     */
+    reduceOrder(target) {
+        for (let order of this.orders) {
+            if (order.name === this.currentObj.name) {
+                order.count--;
+                if (order.count === 0) {
+                    this.deleteOrder(target);
+                }
+            }
+        }
+        this.render();
+    },
+
+    /**
+     * Удаляет товар из массива и отображает количество и цену всех товаров.
+     */
+    deleteOrder() {
+        for (let order of this.orders) {
+            if (order.name === this.currentObj.name) {
+                const idx = this.orders.indexOf(order);
+                this.orders.splice(idx);
+            }
+        }
+        this.render();
+    },
+
+};
+
+window.onload = () => basket.init();
